0365a3e2e9993a494949c2b2054e17c8da6a6362
Gaurav <mahajangaurav2002@gmail.com>
2024-12-17 22:24:59 +0530

    server.js and dockerfile added

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require(‘express’)
+const app = express()
+app.get(‘/’,(request,response) =>{
+response.send(‘welcome to my express application’)
+})
+app.listen(4000, ‘0.0.0.0’ , () =>{
+response.send(‘welcome to my express application’)})
